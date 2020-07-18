fabf6b775586f62434b3015be598ba254e80921f
Deepak Negi <st.deepak15@gmail.com>
2020-07-18 13:01:50 +0530

    Change App.js

--- test_admin/src/App.js
+++ test_admin/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import dataProvider from "./dataProvider";
-// import jsonServerProvider from "ra-data-json-server";
-import Dashboard from "./dashboard";
+import jsonServerProvider from "ra-data-json-server";
+// import Dashboard from "./dashboard";
 import Users from "./users";
 import { PostList, PostEdit, PostCreate } from "./post";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 import authProvider from "./authProvider";
 const App = (props) => {
-  // const dataProvider = jsonServerProvider(
-  //   "https://jsonplaceholder.typicode.com"
-  // );
+  const dataProvider = jsonServerProvider(
+    "https://jsonplaceholder.typicode.com"
+  );
   return (
     <Admin
       authProvider={authProvider}
